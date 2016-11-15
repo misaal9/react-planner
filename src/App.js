f90ac53fbf3cb20b735e3891667e2e3f8f519bda
Misaal <misaal9@gmail.com>
2016-11-15 15:32:29 +0530

    Some changes. Created basic template.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const words = [
+    {
+        'text': 'sample text 1',
+        'size': 10
+    },
+    {
+        'text': 'sample text 2',
+        'size': 10
+    },
+    {
+        'text': 'sample text 3',
+        'size': 10
+    }
+
+]
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+    }
+    
+    addWord(e) {
+        // pevent page reload on submit
+        e.preventDefault();
+
+        // fetch ui to append new word to
+        const ul = document.getElementById('list');
+        const li = document.createElement('li');
+        const input = this.refs.newWord;
+        const newSpan = document.createElement('span');
+        newSpan.innerHTML = input.value;
+        
+        // append word
+        li.appendChild(newSpan);
+        ul.appendChild(li);
+
+        // reset input field
+        input.value = '';
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <h2>Sample header</h2>
+                <form onSubmit={ this.addWord.bind(this) }>
+                    <input type="text" ref="newWord" placeholder="Enter text" /> <button type="button" onClick={ this.addWord.bind(this) } >Click</button>
+                </form>
+                <div id="canvas" className="tag-cloud">
+                    <ul id="list">
+                        <li><span>sample text 1</span></li>
+                        <li><span>sample text 2</span></li>
+                        <li><span>sample text 3</span></li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
